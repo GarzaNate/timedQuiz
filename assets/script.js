987f5294408833cc051049780bd5c8a553067dee
slendyG <josuegarza0608@gmail.com>
2022-07-27 15:14:35 -0700

    Added some of the layout of html.

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var timer = document.querySelector('.timer');
+var sec = document.querySelector('.timer-sec');
+var questions = document.querySelector('.questions');
+var start = document.querySelector('#start-button');
