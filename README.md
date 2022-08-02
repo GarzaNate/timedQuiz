ffae26af18bcdc6e86775b6fdf9feac5dec93972
slendyG <josuegarza0608@gmail.com>
2022-08-02 00:00:33 -0700

    Made changes to the start quiz function, added button elements to the html, and added info to my readMe file.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-https://stackoverflow.com/questions/65992260/how-to-display-5-questions-from-a-javascript-array-at-the-same-time-in-html
+# This is my timed quiz project.
+
+# For this timed quiz, I used mostly JavaScript to handle the functionality of the quiz.
+# I stored my questions and answers in an array that was kept in an object, which I later called to display the question and answers on the screen.
+# I created a timer counting down from 60 seconds, wasn't able to get that to fully function.
+# I was not able to finish this project to completion, planning on a resubmit with a much more finished/polished application.
+
+![Screenshot of my quiz](image1/timedQuiz-screenshot.png)
--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,11 @@ var timer = document.querySelector('.timer');
 var sec = document.querySelector('.timer-sec');
 var questions = document.querySelector('.questions');
 var start = document.querySelector('#start-button');
-
+var btn1 = document.getElementById('answer1')
+var btn2 = document.getElementById('answer2')
+var btn3 = document.getElementById('answer3')
+var btn4 = document.getElementById('answer4')
+var number = 0;
 // stored questions
 var q = [
     {
@@ -54,16 +58,39 @@ strtBtn.addEventListener("click", strtQuiz);
 
 function strtQuiz (event) {
     event.preventDefault();
-    var number = 0;
+    console.log(q[number].question)
+    console.log(q[number].answers)
+    number = 0;
    // var q1 = console.log(q[number].question);
-    var q1 = questions.innerHTML = (q[number].question);
-    var a1 = questions.innerHTML = (q[number].answers);
+    questions.innerHTML = (q[number].question);
+    btn1.innerHTML = q[number].answers[0].text;
+    btn2.innerHTML = q[number].answers[1].text;
+    btn3.innerHTML = q[number].answers[2].text;
+    btn4.innerHTML = q[number].answers[3].text;
+    // var a1 = questions.innerHTML = (q[number].answers);
    // var a1 = console.log(q[number].answers);
     number ++;
 };    
 
 
+var answerOptions = document.getElementById('answer-options');
+answerOptions.addEventListener('click', answerQuestion)
+
+function answerQuestion(event) {
+    console.log(event.target);
+    var userChoice = event.target.id;
+    console.log(userChoice);
+}
+
+
+
 // timer
+
+var gameTimer = setInterval(function () {
+   var sec = Math.floor((60 % (1000 * 60))/ 1000);
+    console.log(sec);
+    sec.innerHTML = timer;
+}, 1000);
 
 // var timeleft = 60;
 // var gameTimer = setInterval(function(){
